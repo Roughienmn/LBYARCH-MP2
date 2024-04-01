e703cc730b8baf2a56eb8c5c8d319f73c865a37c
Dane Chan <dane_chan@dlsu.edu.ph>
2024-04-01 22:52:48 +0800

    Restructured main.c

--- main.c
+++ main.c
@@ -16,10 +16,37 @@ void fillDoubleArray(double* a, int n) {
 extern void daxpy_c(int n, double A, double* X, double* Y, double* Z);
 extern void daxpy_asm(int n, double A, double* X, double* Y, double* Z);
 
-int main() {
-	int n = 20;
-	double A = 2.0;
-	double* X,* Y, * Z_c, * Z_asm;
+void printOutput(int n, double A, double* X, double* Y, double* Z_asm, double* Z_c) {
+	printf("n: %d\n", n);
+	printf("A: %.2f\n", A);
+	printf("Array X:\n%.2f", X[0]);
+	for (int i = 1; i < 10; i++) {
+		printf(", %.2f", X[i]);
+	}
+	printf("\n\n");
+
+	printf("Array Y:\n%.2f", Y[0]);
+	for (int i = 1; i < 10; i++) {
+		printf(", %.2f", Y[i]);
+	}
+	printf("\n\n");
+
+	printf("Array Z_c:\n%.2f", Z_c[0]);
+	for (int i = 1; i < 10; i++) {
+		printf(", %.2f", Z_c[i]);
+	}
+	printf("\n\n");
+
+	printf("Array Z_asm:\n%.2f", Z_asm[0]);
+	for (int i = 1; i < 10; i++) {
+		printf(", %.2f", Z_asm[i]);
+	}
+	printf("\n\n");
+}
+
+void runKernel(int n) {
+	double A = ((double)rand() / (double)(RAND_MAX)) * MAX;
+	double* X, * Y, * Z_c, * Z_asm;
 	X = (double*)malloc(n * sizeof(double));
 	Y = (double*)malloc(n * sizeof(double));
 	Z_c = (double*)malloc(n * sizeof(double));
@@ -31,29 +58,18 @@ int main() {
 	daxpy_c(n, A, X, Y, Z_c);
 	daxpy_asm(n, A, X, Y, Z_asm);
 
-	printf("Array X:\n");
-	for (int i = 0; i < n; i++) {
-		printf("%.2f ", X[i]);
-	}
-	printf("\n");
+	printOutput(n, A, X, Y, Z_asm, Z_c);
+}
 
-	printf("Array Y:\n");
-	for (int i = 0; i < n; i++) {
-		printf("%.2f ", Y[i]);
+void runTests() {
+	int n = 1073741824;
+	int i = 1;
+	for (i = 1; i <= n; i++) {
+		runKernel(i);
+		printf("\n");
 	}
-	printf("\n");
+}
 
-	printf("Array Z_c:\n");
-	for (int i = 0; i < n; i++) {
-		printf("%.2f ", Z_c[i]);
-	}
-	printf("\n");
-
-	printf("Array Z_asm:\n");
-	for (int i = 0; i < n; i++) {
-		printf("%.2f ", Z_asm[i]);
-	}
-	printf("\n");
-
-	return 0;
+int main() {
+	runTests();
 }
